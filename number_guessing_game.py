46aa01a9a4ff7f4b7a3e9caba6dd44f234bc4e11
Tanuj Aswani <tanuj.aswani@gmail.com>
2018-11-03 19:04:54 -0700

    Stylistic Changes

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -4,7 +4,7 @@ global high_score
 
 def start_game():
     # write your code inside this function.
-    print("Welcome to the Number Guessing Game! \n")
+    print("\n\033[1mWelcome to the Number Guessing Game!\033[0m\n")
     random_number = random.randint(1, 10)
     num_guesses = 1
     high_score = 0
@@ -15,13 +15,13 @@ def start_game():
             try:
                 guess = int(input("What do you think the number is?  "))
                 if guess > 10:
-                    print("Please enter a number between 1 and 10 \n")
+                    print("\nPlease enter a number between 1 and 10 \n")
                 elif guess < 1:
-                    print("Please enter a number between 1 and 10 \n")
+                    print("\nPlease enter a number between 1 and 10 \n")
                 else:
                     break
             except ValueError:
-                print("That isn't a number! Please try again... \n")
+                print("\nThat isn't a number! Please try again... \n")
         if guess > random_number:
             print("\n\033[1m The number is lower than your guess! \033[0m\n")
             num_guesses += 1
@@ -32,13 +32,13 @@ def start_game():
             continue
         elif guess == random_number:
             print("\n\033[1mCongrats! You guessed the number! \033[0m\n")
-            print("It took", num_guesses, "tries for you to guess the number!\n")
+            print("It took", num_guesses, "trie(s) for you to guess the number!\n")
             if num_guesses < high_score or high_score == 0:
                 high_score = num_guesses
-            print("Current high score is {}!\n".format(high_score))
             while True:
                 restart = input("The game is now over. Would you like to play again? Y/N  \n")
                 if restart.lower() == "y":
+                    print("\nCurrent high score is {}!\n".format(high_score))
                     num_guesses = 1
                     break
                 elif restart.lower() == "n":
